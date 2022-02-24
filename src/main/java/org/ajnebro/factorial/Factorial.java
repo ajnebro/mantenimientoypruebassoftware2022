a5192a9911cc5813280659095847d55f2d9f7e18
ajnebro <ajnebro@users.noreply.github.com>
2022-02-24 12:28:17 +0100

    Add new test case in class FactorialTest

--- src/main/java/org/ajnebro/factorial/Factorial.java
+++ src/main/java/org/ajnebro/factorial/Factorial.java
@@ -3,6 +3,12 @@ package org.ajnebro.factorial;
 public class Factorial {
 
   public int compute(int value) {
-    return 1 ;
+    int result ;
+    if ((value == 0) || (value == 1)) {
+      result = 1 ;
+    } else {
+      result = 2 ;
+    }
+    return result ;
   }
 }
--- src/test/java/org/ajnebro/factorial/FactorialTest.java
+++ src/test/java/org/ajnebro/factorial/FactorialTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Test cases:
  * factorial 0 -> 1 *
- * factorial 1 -> 1
+ * factorial 1 -> 1 *
  * factorial 2 -> 2
  * factorial 3 -> 6
  */
@@ -19,6 +19,24 @@ class FactorialTest {
     Factorial factorial = new Factorial() ;
     int expectedValue = 1 ;
     int obtainedValue = factorial.compute(0) ;
+
+    assertEquals(expectedValue, obtainedValue) ;
+  }
+
+  @Test
+  public void shouldComputeReturnOneIfTheNumberIsOne() {
+    Factorial factorial = new Factorial() ;
+    int expectedValue = 1 ;
+    int obtainedValue = factorial.compute(1) ;
+
+    assertEquals(expectedValue, obtainedValue) ;
+  }
+
+  @Test
+  public void shouldComputeReturnTwoIfTheNumberIsTwo() {
+    Factorial factorial = new Factorial() ;
+    int expectedValue = 2 ;
+    int obtainedValue = factorial.compute(2) ;
 
     assertEquals(expectedValue, obtainedValue) ;
   }
