344ebb02a0ea26aa57b1757906da58fb9095fd6a
ajnebro <ajnebro@users.noreply.github.com>
2022-02-25 11:16:31 +0100

    Update class FactorialTest with new test cases

--- src/test/java/org/ajnebro/factorial/FactorialTest.java
+++ src/test/java/org/ajnebro/factorial/FactorialTest.java
@@ -1,15 +1,20 @@
 package org.ajnebro.factorial;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileStore;
 import org.junit.jupiter.api.Test;
 
 /**
  * Test cases:
  * factorial 0 -> 1 *
  * factorial 1 -> 1 *
- * factorial 2 -> 2
- * factorial 3 -> 6
+ * factorial 2 -> 2 *
+ * factorial 3 -> 6 *
+ * factorial 6 -> 720 *
+ * factorial negative number -> raises an exception
  */
 
 class FactorialTest {
@@ -25,7 +30,7 @@ class FactorialTest {
 
   @Test
   public void shouldComputeReturnOneIfTheNumberIsOne() {
-    Factorial factorial = new Factorial() ;
+    var factorial = new Factorial() ;
     int expectedValue = 1 ;
     int obtainedValue = factorial.compute(1) ;
 
@@ -34,10 +39,34 @@ class FactorialTest {
 
   @Test
   public void shouldComputeReturnTwoIfTheNumberIsTwo() {
-    Factorial factorial = new Factorial() ;
+    var factorial = new Factorial() ;
     int expectedValue = 2 ;
     int obtainedValue = factorial.compute(2) ;
 
     assertEquals(expectedValue, obtainedValue) ;
+  }
+
+  @Test
+  public void shouldComputeReturnSixIfTheNumberIsThree() {
+    var factorial = new Factorial() ;
+    int expectedValue = 6 ;
+    int obtainedValue = factorial.compute(3) ;
+
+    assertEquals(expectedValue, obtainedValue) ;
+  }
+
+  @Test
+  public void shouldComputeReturn720IfTheNumberIs6() {
+    var factorial = new Factorial() ;
+    int expectedValue = 720 ;
+    int obtainedValue = factorial.compute(6) ;
+
+    assertEquals(expectedValue, obtainedValue) ;
+  }
+
+  @Test
+  public void shouldComputeOfANegativeNumberRaiseAnException() {
+    var factorial = new Factorial() ;
+    assertThrows(RuntimeException.class, () -> factorial.compute(0)) ;
   }
 }
